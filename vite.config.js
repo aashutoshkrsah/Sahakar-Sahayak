f21048103471bcfb899925a6dad77a3d60ffbb1f
Anadijain1234 <anadijain24@gmail.com>
2026-09-10 00:08:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
@@ -10,6 +9,7 @@ export default defineConfig({
       '/api': {
         target: 'https://sahakar-sahayak-2.onrender.com',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
